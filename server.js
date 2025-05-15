3d4fc5cfe8c1022b24f13048ffe2a1b8f0ae7876
Mohammed-Sayed1 <farsfhd123@gmail.com>
2025-05-15 17:38:42 +0300

    Added heroku config

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-process.on('uncaughtException', err => {
-  console.log('UNHANDLED EXCEPTION! 💥 Shutting down...')
-  console.log(err.name, err.message)
-  process.exit(1)
-})
+process.on('uncaughtException', (err) => {
+  console.log('UNHANDLED EXCEPTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
 
 dotenv.config({ path: 'config.env' });
 const app = require('./app');
@@ -18,7 +18,7 @@ const DB = process.env.DATABASE.replace(
 
 mongoose
   .connect(
-    DB
+    DB,
     // ,
     // /** these are some configurations to deal with deprecation warnings */
     // /** no need for them after mongoose 7 */
@@ -40,10 +40,17 @@ const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
 
-process.on('unhandledRejection', err => {
-  console.log('UNHANDLED REJECRION! 💥 Shutting down...')
-  console.log(err.name, err.message)
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECRION! 💥 Shutting down...');
+  console.log(err.name, err.message);
   server.close(() => {
-    process.exit(1)
-  })
-})
+    process.exit(1);
+  });
+});
+
+process.on('SIGTERM', () => {
+  console.log(' 👋 SIGTERM RECEIVED. Shutting down gracefully');
+  server.close(() => {
+    console.log('💥 Process terminated!');
+  });
+});
