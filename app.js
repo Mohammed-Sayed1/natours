13c63b24f6dfd67b9c8dd5f4f37318d1ff5bc70c
Mohammed-Sayed1 <farsfhd123@gmail.com>
2025-05-20 16:47:19 +0300

    improved stripe implementation

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const bookingRouter = require('./routes/bookingRoutes');
+const bookingController = require('./controllers/bookingController');
 const viewRouter = require('./routes/viewRoutes');
 
 const app = express();
@@ -36,7 +37,7 @@ app.set('views', path.join(__dirname, 'views'));
 //* Implement CORS
 app.use(cors()); //* cors() adds this header 'Access-Control-Allow-Origin' to * (all incoming requests)
 
-app.options('*', cors())
+app.options('*', cors());
 
 //* Serving static files
 app.use(express.static(path.join(__dirname, 'public'))); //* this is how to serve static files from a folder not a route.
@@ -94,6 +95,12 @@ const limiter = rateLimit({
   message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
+
+app.post(
+  '/webhook-checkout',
+  express.raw({ type: 'application/json' }),
+  bookingController.webhookCheckout,
+);
 
 //* Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
